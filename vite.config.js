2dc91af74eb4642b255d0c64338e982a177f7f24
step135 <mmir@seznam.cz>
2023-12-16 21:54:48 +0100

    Old browsers supported

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [legacy({
+    targets: ['defaults', 'IE 11'],
+  }),svelte()],
 })
